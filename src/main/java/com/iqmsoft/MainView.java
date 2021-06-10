6f1fa20190a58e39b9b3f47a7c62f028fb68b815
Muruga <davanon2014@gmail.com>
2021-06-10 10:48:37 +0530

    First

---
+++ src/main/java/com/iqmsoft/MainView.java
@@ -0,0 +1,22 @@
+package com.iqmsoft;
+
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.notification.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+@HtmlImport("css/shared-styles.html")
+public class MainView extends VerticalLayout {
+
+    public MainView(@Autowired MessageBean bean) {
+        Button button = new Button("Click me",
+                e -> Notification.show(bean.getMessage()));
+        add(button);
+    }
+
+}
